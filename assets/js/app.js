920006c1ab267dba42d7808e5b6b09aad103b2d8
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-11-22 15:29:00 -0600

    setup of svg area

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
 // @TODO: YOUR CODE HERE!
+
+// Starting from 16.3.2 example ... copying directly the arrangement for svg setup
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.csv("hours-of-tv-watched.csv").then(function (tvData) {
+
+
+}
