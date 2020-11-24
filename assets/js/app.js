5d6c5d4e964112f4b9f6fb535b6ffe08a4648d30
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-11-24 16:58:05 -0600

    post tutor session 11-24

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // @TODO: YOUR CODE HERE!
-
+// "Boilerplate"
 // Starting from 16.3.2 example ... copying directly the arrangement for svg setup
 // Define SVG area dimensions, basically chart sizing from here to 30
 var svgWidth = 960;
@@ -31,13 +31,132 @@ var chartGroup = svg.append("g")
 
 // Load data 
 d3.csv("assets/data/data.csv").then(function (Data) {
-    console.log(Data);
+    // console.log(Data);
+
+    Data.forEach(function (State) {
+
+        // Parse data
+        State.obesity = +State.obesity;
+        State.poverty = +State.poverty;
+        State.smokes = +State.smokes;
+
+        console.log(State)
+
+    });
+
+
+    // set up X scale
+    var xScale = d3.scaleLinear()
+        .domain(d3.extent(Data, d => d.obesity))
+        .range([0, chartWidth]);
+
+
+    // set up y scale 
+    var yScale = d3.scaleLinear()
+        .domain(d3.extent(Data, d => d.smokes))
+        .range([chartHeight, 0]);
+
+
+    // ==============================
+    // axis bottom / left actually draws the line for the axis 
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
+
+    // ==============================
+    // append = create new element, create another g within it (go to the bottom of the chart)
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+
+    // do not need to move Y axis left/right, only (maybe) flip
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circles")
+        .data(Data)
+        // since there are none, we've created 52 placeholders that are now 'appendable'
+        .enter()
+
+    circlesGroup
+        .append("circle")
+        .attr("cx", d => xScale(d.obesity))
+        .attr("cy", d => yScale(d.smokes))
+        .attr("r", "15")
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+
+    // creating the state text within bubble
+    var circlesGroupText = circlesGroup
+        .append("text")
+        .text(d => d.abbr)
+        .attr("dx", d => xScale(d.obesity))
+        .attr("dy", d => yScale(d.smokes))
+        .classed("stateText", true);
+
+
+
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function (d) {
+            return (`${d.obesity}<br>State: ${d.state}<br>Smokes: ${d.smokes}`);
+        });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function (Data) {
+        toolTip.show(Data, this);
+    })
+        // onmouseout event
+        .on("mouseout", function (Data, index) {
+            toolTip.hide(Data);
+        });
+
+    // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - chartMargin.left + 40)
+        .attr("x", 0 - (chartHeight / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Percentage of Smokers");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Hair Metal Band Hair Length (inches)");
+
 
 }).catch(function (error) {
     console.log(error);
 });
 
 
+
+
+
+
+
+
+
+// // set up X scale
+// var xScale = d3.scaleLinear()
+//     .domain(d3.extent(Data, d => d.poverty))
+//     .range([0, svgWidth])
+
+
+// // set up y scale 
+// var yScale = d3.scaleLinear()
+//     .domain(d3.extent(Data, d => d.smokes))
+//     .range([svgHeight, 0])
 
     // // log a list of names
     // var names = Data.map(data => data.name);
